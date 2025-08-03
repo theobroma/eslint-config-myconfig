ab0105e525708e1487fc72e93d3553eaf42da927
Alexandr Siryj <theobroma222@gmail.com>
2025-08-03 16:25:40 +0300

    add rules

--- index.js
+++ index.js
@@ -9,6 +9,27 @@ export default [
         rules: {
             '@typescript-eslint/no-explicit-any': 1,
             '@typescript-eslint/no-unused-vars': 1,
+            '@typescript-eslint/no-use-before-define': 'error',
+            '@typescript-eslint/naming-convention': [
+                'error',
+                {
+                    selector: 'variable',
+                    format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+                },
+                {
+                    selector: 'variable',
+                    types: ['boolean'],
+                    format: ['PascalCase'],
+                    prefix: ['is', 'should', 'has', 'can', 'did', 'will'],
+                },
+                {
+                    selector: 'enum',
+                    format: ['PascalCase'],
+                    suffix: ['Enum'],
+                },
+            ],
+
+            'no-use-before-define': 'off', // Note: you must disable the base rule as it can report incorrect errors; use @typescript-eslint/no-use-before-define
         },
     },
 ];
